bddf0aa7deef554c17289e278247b8a810abc16b
oludare89 <azizoyekan@hotmail.com>
2022-10-08 12:56:19 +0000

    Added remaining animation objects

--- script.js
+++ script.js
@@ -7,8 +7,6 @@ const playerImage = new Image();
 playerImage.src = "images/shadow_dog.png";
 const spriteWidth = 575;
 const spriteHeight = 523;
-let frameX = 0;
-let frameY = 0;
 let gameFrame = 0;
 const staggerFrames = 5; //higher the staggerFrames the faster the animation
 const spriteAnimations = [];
@@ -20,7 +18,39 @@ const animationStates = [
     {
         name: 'jump',
         frames: 7,
-    }
+    },
+    {
+        name: 'fall',
+        frames: 7,
+    },
+    {
+        name: 'run',
+        frames: 9,
+    },
+    {
+        name: 'dizzy',
+        frames: 11,
+    },
+    {
+        name: 'sit',
+        frames: 5,
+    },
+    {
+        name: 'roll',
+        frames: 7,
+    },
+    {
+        name: 'bite',
+        frames: 7,
+    },
+    {
+        name: 'ko',
+        frames: 12,
+    },
+    {
+        name: 'getHit',
+        frames: 4,
+    },
 ];
 
 animationStates.forEach((state, index) => {
@@ -38,9 +68,9 @@ console.log(spriteAnimations);
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    let position = Math.floor(gameFrame/staggerFrames) % 6;
-    frameX = spriteWidth * position;
-    frameY = spriteHeight * position;
+    let position = Math.floor(gameFrame/staggerFrames) % spriteAnimations[].loc.length;
+    let frameX = spriteWidth * position;
+    let frameY = spriteAnimations[].loc[position].y;
     ctx.drawImage(playerImage, frameX, frameY, 
         spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
     
